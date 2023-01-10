213b917112ef154974a368da28fa93b412551f61
Mark Bailey <markbailey78@yahoo.co.uk>
2023-01-10 22:22:04 +0000

    Create useTimeout.test.ts

---
+++ packages/react-toolkit-hooks/src/__tests__/useTimeout.test.ts
@@ -0,0 +1,28 @@
+import { act, renderHook, waitFor } from '@testing-library/react';
+import useTimeout from '../hooks/useTimeout';
+
+describe('useTimeout', () => {
+  test('Verify the timeout triggers the callback', async () => {
+    const timeoutFn = jest.fn();
+    renderHook(() => useTimeout(timeoutFn, 100));
+    await waitFor(() => expect(timeoutFn).toBeCalled());
+  });
+
+  test('Verify the timeout gets cleared', async () => {
+    const timeoutFn = jest.fn();
+    const { result } = renderHook(() => useTimeout(timeoutFn, 1000));
+    const { clear } = result.current;
+
+    act(() => clear());
+    await waitFor(() => expect(timeoutFn).not.toHaveBeenCalled());
+  });
+
+  test('Verify the timeout gets reset', async () => {
+    const timeoutFn = jest.fn();
+    const { result } = renderHook(() => useTimeout(timeoutFn, 500));
+    const { reset } = result.current;
+
+    act(() => reset());
+    await waitFor(() => expect(timeoutFn).toBeCalledTimes(1));
+  });
+});
