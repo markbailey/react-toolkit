41b8b901d1fb4409723846305094aadd006901c1
Mark Bailey <markbailey78@yahoo.co.uk>
2023-01-10 12:47:12 +0000

    Create react-toolkit-core package initial repo

---
+++ packages/react-toolkit-core/rollup.config.js
@@ -0,0 +1,43 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import json from '@rollup/plugin-json';
+import url from '@rollup/plugin-url';
+import dts from 'rollup-plugin-dts';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import { terser } from 'rollup-plugin-terser';
+
+const packageJson = require('./package.json');
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true
+      }
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      json(),
+      url(),
+      commonjs(),
+      typescript({ tsconfig: './tsconfig.json' }),
+      terser()
+    ]
+  },
+  {
+    input: 'dist/esm/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()],
+    external: [/\.css$/]
+  }
+];
