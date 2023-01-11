4ccfa2340050b570060932d30c22fab0d3cb28bf
Mark Bailey <markbailey78@yahoo.co.uk>
2023-01-11 22:55:31 +0000

    Update exports of hooks with useCopyToClipboard and useUpdateEffect

--- packages/react-toolkit-hooks/src/index.ts
+++ packages/react-toolkit-hooks/src/index.ts
@@ -1,7 +1,9 @@
 export { default as useClickOutside } from './hooks/useClickOutside';
+export { default as useCopyToClipboard } from './hooks/useCopyToClipboard';
 export { default as useCookie } from './hooks/useCookie';
 export { default as useDebounce } from './hooks/useDebounce';
 export { default as useEventListener } from './hooks/useEventListener';
 export { default as useMediaQuery } from './hooks/useMediaQuery';
 export { useLocalStorage, useSessionStorage } from './hooks/useStorage';
 export { default as useTimeout } from './hooks/useTimeout';
+export { default as useUpdateEffect } from './hooks/useUpdateEffect';
